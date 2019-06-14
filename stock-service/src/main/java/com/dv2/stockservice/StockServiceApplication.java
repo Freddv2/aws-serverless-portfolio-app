aa2499f494a00482e0bf660f10a1cace6084eb5f
Freddv2 <fredddv@gmail.com>
2019-06-14 07:58:31 -0400

    stock-service spring init

---
+++ stock-service/src/main/java/com/dv2/stockservice/StockServiceApplication.java
@@ -0,0 +1,13 @@
+package com.dv2.stockservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StockServiceApplication.class, args);
+    }
+
+}
