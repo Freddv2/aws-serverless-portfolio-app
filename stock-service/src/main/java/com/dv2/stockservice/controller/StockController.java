08897d4e5a92e00ed3767dc5e809e8d3835791e5
Freddv2 <fredddv@gmail.com>
2019-07-14 20:54:28 -0400

    Refactored stock-service to be serverless and aws lambda ready

--- stock-service/src/main/java/com/dv2/stockservice/controller/StockController.java
+++ stock-service/src/main/java/com/dv2/stockservice/controller/StockController.java
@@ -6,36 +6,34 @@ import com.dv2.stockservice.service.StockService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @RestController
-@RequestMapping("/stock-service")
-@CrossOrigin(origins = "http://localhost")
-public class StockServiceController {
-
-    Logger LOGGER = LoggerFactory.getLogger(StockServiceController.class);
+@EnableWebMvc
+public class StockController {
 
     private final StockService stockService;
+    Logger LOGGER = LoggerFactory.getLogger(StockController.class);
 
     @Autowired
-    public StockServiceController(StockService stockService) {
+    public StockController(StockService stockService) {
         this.stockService = stockService;
     }
 
-    @GetMapping("/stocks/{symbol}")
-    public ResponseEntity<Stock> findStock(@PathVariable("symbol") final String symbol) {
+    @RequestMapping(path = "/stock/{symbol}", method = RequestMethod.GET)
+    public Stock findStock(@PathVariable("symbol") String symbol) {
         Stock stock = new Stock(symbol);
         try {
             yahoofinance.Stock yahooStock = stockService.findStock(symbol, false);
             stock.setName(yahooStock.getName());
             stock.setPrice(yahooStock.getQuote().getPrice());
-            return new ResponseEntity<>(stock, HttpStatus.OK);
         } catch (StockNotFoundException e) {
             LOGGER.error("Stock " + symbol + " not found");
-            return new ResponseEntity<>(stock, HttpStatus.NOT_FOUND);
         }
+        return stock;
     }
 }
--- stock-service/src/test/java/com/dv2/stockservice/StockServiceApplicationTests.java
+++ stock-service/src/test/java/com/dv2/stockservice/StockServiceApplicationTests.java
@@ -2,15 +2,13 @@ package com.dv2.stockservice;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
 public class StockServiceApplicationTests {
 
     @Test
-    public void contextLoads() {
-    }
+    public void findStock() {
 
+    }
 }
